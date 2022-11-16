3cb530492ff0fff3b3174fc9e5af3f07e021771f
Vanessa Neves <113128675+vanessaneves-dev@users.noreply.github.com>
2022-11-15 21:58:44 -0300

    :goal_net: correção de erro

--- js/script.js
+++ js/script.js
@@ -1,13 +1,30 @@
-const links = document.querySelectorAll(".header-menu a");
 
-function ativarLink(link) {
-  const url = location.href;
-  const href = link.href;
+function incLink() {
+  const links = document.querySelectorAll(".header-menu a");
 
-  if (url.includes(href)) {
-    link.classList.add("ativo");
+  function ativarLink(link) {
+    const url = location.href;
+    const href = link.href;
+
+    if (url.includes(href)) {
+      link.classList.add("ativo");
+    }
   }
+
+  links.forEach(ativarLink);
+}
+incLink();
+
+
+
+const termoList = document.querySelectorAll('.termos h2');
+
+termoList[0].nextElementSibling.classList.add('ativo');
+
+function activeLink() {
+  this.nextElementSibling.classList.toggle('ativo');
 }
 
-links.forEach(ativarLink);
-
+termoList.forEach((item) => {
+  item.addEventListener('click', activeLink);
+})
